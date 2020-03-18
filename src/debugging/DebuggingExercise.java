529b6a12aa4075d646036d47ab4df2d786994bb3
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-18 18:33:44 +0100

    DebuggingExercise class added

---
+++ src/debugging/DebuggingExercise.java
@@ -0,0 +1,45 @@
+package debugging;
+
+public class DebuggingExercise {
+    public static void main(String[] args) {
+       
+        //create an int array we want to sort using bubble sort algorithm
+        int array[] = new int[]{5,0,-35,45,150,3};
+           
+        //print array before sorting using bubble sort algorithm
+        System.out.println("Original array");
+        for(int el : array){
+            System.out.print(el + " ");
+        }
+           
+        sort(array);
+           
+        System.out.println("");
+           
+        //print array after sorting using bubble sort algorithm
+        System.out.println("Sorted array");
+        for(int el : array){
+            System.out.print(el + " ");
+        }
+    }
+       
+    private static void sort(int[] array) {
+           
+        int n = array.length;
+        int temp = 0;
+           
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                   
+                if(array[j-1] > array[j]){
+                    //swap the elements!
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[i] = temp;
+                }
+            }
+        }
+       
+    }
+}
+
